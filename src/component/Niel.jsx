0f734504f8e14ab48d825dc15f43390fb9f0496b
Ayenidaniel <ayenimayokun@gmail.com>
2023-04-13 15:26:20 +0100

    secrid

---
+++ src/component/Niel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+
+ // owl carousel settings
+const options = {
+    loop:true,
+    items:3,
+    margin:0,
+    autoplay:false,
+    dots:true,
+    autoPlayTimeout:8500,
+    smartSpeed:450,
+    nav:false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        
+       
+       
+    }
+
+};
+
+const Niel = () => {
+    return(
+
+        <OwlCarousel className= " owl-carousel owl-theme    " {...options}>
+
+        
+        
+                       
+        </OwlCarousel>
+       
+    );
+}
+
+export default Niel;
